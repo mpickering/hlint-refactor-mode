0626ad9cc392a29431eb3888bd2c5689889308d7
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2015-09-21 15:20:32 +0200

    Add some documentation and clean up the code

--- hlint-refactor-mode.el
+++ hlint-refactor-mode.el
@@ -1,43 +1,74 @@
-(defun refact-gen (opts)
-    (shell-command-on-region-with-output-to-end-of-buffer (point-min) (point-max) (concat "hlint --refactor - " opts))
-      )
+;; hlint-refactor-mode.el -- Apply HLint suggestions
 
-(defun shell-command-on-region-to-string (start end command)
-  (with-output-to-string
-    (shell-command-on-region start end command standard-output)))
+;; Copyright (C) 2015 Matthew Pickering
 
-  
-(defun call-process-region-checked (start end program)
-  (let ((exit (call-process-region
-	      start end
-	      shell-file-name      ; name of program
-	      1            ; delete region
-	      t            ; send output to buffer
-	      nil          ; no redisplay during output
-	      "-c" program
-	      )))
+;; Author Matthew Pickering
+;; Keywords: haskell, refactor
+;; URL: https://github.com/mpickering/hlint-refactor-mode
+
+;;; Commentary:
+
+;; This package provides a minor mode to apply the suggestions from
+;; hlint.
+;;
+;; To activate it use (add-hook 'haskell-mode-hook 'hlint-refactor-mode).
+
+;;; Code:
+
+(defun call-process-region-checked (start end program &optional args)
+  "Send text from START to END to PROGRAM with ARGS.
+This is a wrapper around `call-process-region' that doesn't replace
+the region with the output of PROGRAM if it returned a non-zero
+exit code."
+  (let ((exit (apply 'call-process-region
+                     start end
+                     program            ; name of program
+                     t                  ; delete region
+                     t                  ; send output to buffer
+                     nil                ; no redisplay during output
+                     args
+                     )))
     (unless (eq exit 0) (primitive-undo 1 buffer-undo-list))))
 
 
-(defun shell-command-on-region-with-output-to-end-of-buffer (start end command)
+(defun call-process-region-preserve-point (start end program &optional args)
+  "Send text from START to END to PROGRAM with ARGS preserving the point.
+This uses `call-process-region-checked' internally."
+  (let ((oldpoint (point)))
+    (call-process-region-checked start end program args)
+    (goto-char oldpoint)))
+
+;;;###autoload
+(defun hlint-refactor (&optional args)
+  "Apply all hlint suggestions in the current buffer.
+ARGS specifies additional arguments that are passed to hlint."
   (interactive)
-    (setq pos (point))
-    (call-process-region-checked start end command)
-    (goto-char pos)
-    )
+  (call-process-region-preserve-point
+   (point-min)
+   (point-max)
+   "hlint"
+   (append '("--refactor"
+             "-")
+           args)))
 
-(defun refactor-at-pos () 
-  (setq col (number-to-string (current-column)))
-  (setq line (number-to-string (line-number-at-pos)))
-  (refact-gen
-   (concat "--refactor-options=--pos\\ " line "," col)))
+;;;###autoload
+(defun hlint-refactor-at-point ()
+  "Apply the hlint suggestion at point."
+  (interactive)
+  (let ((col (number-to-string (current-column)))
+        (line (number-to-string (line-number-at-pos))))
+    (hlint-refactor
+     (list (concat "--refactor-options=--pos " line "," col)))))
 
+;;;###autoload
 (define-minor-mode hlint-refactor-mode
   "Automatically apply hlint suggestions"
   :lighter " hlint-refactor"
   :keymap (let ((map (make-sparse-keymap)))
-	    (define-key map (kbd "C-c a") (lambda () (interactive) (refact-gen "")))
-	    (define-key map (kbd "C-c o") (lambda ()
-					    (interactive)
-					    (refactor-at-pos)))
-	    map))
+            (define-key map (kbd "C-c a") 'hlint-refactor)
+            (define-key map (kbd "C-c o") 'hlint-refactor-at-point)
+            map))
+
+(provide 'hlint-refactor-mode)
+
+;;; hlint-refactor-mode.el ends here
