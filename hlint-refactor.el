f5c9efd4bea6b0ef57d162b85bc9bb8b0a9003f8
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2015-11-24 21:41:56 +0100

    Rename to hlint-refactor and prefix functions

--- hlint-refactor.el
+++ hlint-refactor.el
@@ -1,4 +1,4 @@
-;;; hlint-refactor-mode.el --- Apply HLint suggestions
+;;; hlint-refactor.el --- Apply HLint suggestions
 
 ;; Copyright (C) 2015 Matthew Pickering
 
@@ -16,7 +16,7 @@
 
 ;;; Code:
 
-(defun call-process-region-checked (start end program &optional args)
+(defun hlint-refactor-call-process-region-checked (start end program &optional args)
   "Send text from START to END to PROGRAM with ARGS.
 This is a wrapper around `call-process-region' that doesn't replace
 the region with the output of PROGRAM if it returned a non-zero
@@ -32,21 +32,21 @@ exit code."
     (unless (eq exit 0) (primitive-undo 1 buffer-undo-list))))
 
 
-(defun call-process-region-preserve-point (start end program &optional args)
+(defun hlint-refactor-call-process-region-preserve-point (start end program &optional args)
   "Send text from START to END to PROGRAM with ARGS preserving the point.
 This uses `call-process-region-checked' internally."
   (let ((line (line-number-at-pos))
         (column (current-column)))
-    (call-process-region-checked start end program args)
+    (hlint-refactor-call-process-region-checked start end program args)
     (goto-line line)
     (move-to-column column)))
 
 ;;;###autoload
-(defun hlint-refactor (&optional args)
+(defun hlint-refactor-refactor-buffer (&optional args)
   "Apply all hlint suggestions in the current buffer.
 ARGS specifies additional arguments that are passed to hlint."
   (interactive)
-  (call-process-region-preserve-point
+  (hlint-refactor-call-process-region-preserve-point
    (point-min)
    (point-max)
    "hlint"
@@ -55,12 +55,12 @@ ARGS specifies additional arguments that are passed to hlint."
            args)))
 
 ;;;###autoload
-(defun hlint-refactor-at-point ()
+(defun hlint-refactor-refactor-at-point ()
   "Apply the hlint suggestion at point."
   (interactive)
   (let ((col (number-to-string (+ 1 (current-column))))
         (line (number-to-string (line-number-at-pos))))
-    (hlint-refactor
+    (hlint-refactor-refactor-buffer
      (list (concat "--refactor-options=--pos " line "," col)))))
 
 ;;;###autoload
@@ -68,10 +68,10 @@ ARGS specifies additional arguments that are passed to hlint."
   "Automatically apply hlint suggestions"
   :lighter " hlint-refactor"
   :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "C-c , b") 'hlint-refactor)
-            (define-key map (kbd "C-c , r") 'hlint-refactor-at-point)
+            (define-key map (kbd "C-c , b") 'hlint-refactor-refactor-buffer)
+            (define-key map (kbd "C-c , r") 'hlint-refactor-refactor-at-point)
             map))
 
-(provide 'hlint-refactor-mode)
+(provide 'hlint-refactor)
 
 ;;; hlint-refactor-mode.el ends here
